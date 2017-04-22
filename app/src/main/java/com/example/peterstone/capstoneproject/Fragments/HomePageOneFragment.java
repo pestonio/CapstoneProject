b38e2cffd1b58d11eaba9be20c1c38ed13494119
pestonio <pestonio@gmail.com>
2017-04-23 00:12:54 +0100

    Current location details called successfully - town/city, ID, photo

--- app/src/main/java/com/example/peterstone/capstoneproject/Fragments/HomePageOneFragment.java
+++ app/src/main/java/com/example/peterstone/capstoneproject/Fragments/HomePageOneFragment.java
@@ -4,19 +4,23 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -33,10 +37,15 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResult;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -49,8 +58,9 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
     private GoogleApiClient mApiClient;
     private final static int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private LocationRequest mLocationRequest;
-
-    TextView userLocation;
+    private int permissionRequests =0;
+    private TextView mUserLocation;
+    private ImageView mImageView;
 
     public HomePageOneFragment() {
         // Required empty public constructor
@@ -59,59 +69,55 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.current_location_fragment, container, false);
+        View view = inflater.inflate(R.layout.current_location_fragment, container, false);
+        SupportPlaceAutocompleteFragment supportPlaceAutocompleteFragment = new SupportPlaceAutocompleteFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.autocomplete_container, supportPlaceAutocompleteFragment);
+        transaction.commit();
+        supportPlaceAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName());
+                Log.i(TAG, "Place ID: " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+        return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String savedLocation = sharedPreferences.getString(getString(R.string.last_known_current_location), null);
-        Log.v(TAG, "onResume location: " + savedLocation);
-        userLocation = (TextView) getActivity().findViewById(R.id.current_place_name);
+
+        mUserLocation = (TextView) getActivity().findViewById(R.id.current_place_name);
+        mImageView = (ImageView) getActivity().findViewById(R.id.current_place_image);
         final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.swipe_refresh_layout);
+
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                if (mApiClient != null && mApiClient.isConnected()){
+                if (mApiClient != null && mApiClient.isConnected()) {
                     getCurrentLocation();
                     swipeRefreshLayout.setRefreshing(false);
 
-                }else {
+                } else {
                     buildGoogleApi();
                     mApiClient.connect();
                     swipeRefreshLayout.setRefreshing(false);
                 }
             }
         });
-        if (savedLocation == null || savedLocation.equals(getString(R.string.unknown_location))) {
-            buildGoogleApi();
-            mApiClient.connect();
-            Log.v(TAG, "onResume build called");
-        } else {
-            updateLocationName(savedLocation);
-        }
+        buildGoogleApi();
+        mApiClient.connect();
         mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
                 .setInterval(10 * 1000)
                 .setFastestInterval(1000);
-        
-        SupportPlaceAutocompleteFragment autocompleteFragment = (SupportPlaceAutocompleteFragment)
-                getActivity().getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName());
-            }
-
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
-            }
-        });
+        Log.i(TAG, "onResume Called");
     }
 
     protected synchronized void buildGoogleApi() {
@@ -131,6 +137,7 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
         if (mApiClient != null && mApiClient.isConnected()) {
             mApiClient.stopAutoManage(getActivity());
             mApiClient.disconnect();
+            mApiClient = null;
         }
     }
 
@@ -142,7 +149,9 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.v(TAG, "Connected");
-        getCurrentLocation();
+        if (permissionRequests < 10) {
+            getCurrentLocation();
+        }
     }
 
     @Override
@@ -165,41 +174,48 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
 
     public void getCurrentLocation() {
         String currentPlace = null;
-        if (mApiClient.isConnected()) {
-            if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-            } else {
-                Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
-                Log.v(TAG, "The last location is: " + mLastLocation);
-                if (mLastLocation != null) {
-                    double latitude = mLastLocation.getLatitude();
-                    double longitude = mLastLocation.getLongitude();
-                    Geocoder geocoder = new Geocoder(getActivity().getApplicationContext(), Locale.getDefault());
-                    try {
-                        List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
-                        if (addresses.size() > 0) {
-                            String locality = addresses.get(0).getLocality();
-                            String country = addresses.get(0).getCountryName();
-                            Log.v(TAG, "Address Data: " + locality + " + " + country);
-                            currentPlace = locality + ", " + country;
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            permissionRequests ++;
+            Log.i(TAG, "Permission Requested: " + permissionRequests);
+        }
+        else if (mApiClient.isConnected()) {
+            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+            Log.v(TAG, "The last location is: " + mLastLocation);
+            if (mLastLocation != null) {
+                double latitude = mLastLocation.getLatitude();
+                double longitude = mLastLocation.getLongitude();
+                Geocoder geocoder = new Geocoder(getActivity().getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                    if (addresses.size() > 0) {
+                        String locality = addresses.get(0).getLocality();
+                        String country = addresses.get(0).getCountryName();
+                        Log.v(TAG, "Address Data: " + locality + " + " + country);
+                        currentPlace = locality + " - " + country;
+                        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+                        String savedLocation = sharedPreferences.getString(getString(R.string.last_known_current_location), null);
+                        if (currentPlace.equals(savedLocation)) {
+                            Log.i(TAG, "sharedPref and currentPlace Match");
+                            updateLocationName(currentPlace);
+                            return;
+                        } else if (!currentPlace.equals(savedLocation)) {
+                            getCurrentPlaceId(currentPlace);
+                            addLocationToSharedPreferences(currentPlace);
+                            updateLocationName(currentPlace);
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-                }
-                if (mLastLocation == null) {
-                    LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient, mLocationRequest, this);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            Log.v(TAG, "The current place is: " + currentPlace);
-            if (currentPlace == null) {
-                currentPlace = getString(R.string.unknown_location);
+            if (mLastLocation == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient, mLocationRequest, this);
             }
         }
-        getLocationJsonResponse(currentPlace);
-        addLocationToSharedPreferences(currentPlace);
+        Log.v(TAG, "The current place is: " + currentPlace);
     }
 
     private void addLocationToSharedPreferences(String currentPlace) {
@@ -221,17 +237,32 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
         if (currentPlace == null) {
             currentPlace = getString(R.string.unknown_location);
         }
-        userLocation.setText(currentPlace);
+        mUserLocation.setText(currentPlace);
     }
 
-    private void getLocationJsonResponse(String location) {
+    private void getCurrentPlaceId(String location) {
         if (location != null && !location.equals(getString(R.string.unknown_location))) {
             RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-            String url = getString(R.string.webservice_base_url) + getString(R.string.webservice_api_key) + getString(R.string.webservice_query) + location.replaceAll(" ", "%20");
+            String url = getString(R.string.webservice_base_url)
+                    + getString(R.string.webservice_api_key)
+                    + getString(R.string.webservice_query)
+                    + location.replaceAll(" ", "%20");
             JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    Log.v(TAG, "JSON Response: " + response.toString());
+                    try {
+                        JSONArray results = response.getJSONArray("results");
+                        JSONObject resultsObject = results.getJSONObject(0);
+                        String placeId = resultsObject.getString("place_id");
+                        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(getString(R.string.current_place_id), placeId);
+                        editor.apply();
+                        Log.v(TAG, placeId);
+                        placePhotosTask(placeId);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     //TODO parse JSON response and pull out ID and image, update UI.
                 }
             }, new Response.ErrorListener() {
@@ -242,5 +273,88 @@ public class HomePageOneFragment extends Fragment implements GoogleApiClient.Con
             });
             queue.add(objectRequest);
         }
+    }
+
+    abstract class PhotoTask extends AsyncTask<String, Void, PhotoTask.AttributedPhoto> {
+
+        private int mHeight;
+
+        private int mWidth;
+
+        public PhotoTask(int width, int height) {
+            mHeight = height;
+            mWidth = width;
+        }
+
+        @Override
+        protected AttributedPhoto doInBackground(String... params) {
+            if (params.length != 1) {
+                return null;
+            }
+            final String placeId = params[0];
+            AttributedPhoto attributedPhoto = null;
+
+            PlacePhotoMetadataResult result = Places.GeoDataApi
+                    .getPlacePhotos(mApiClient, placeId).await();
+
+            if (result.getStatus().isSuccess()) {
+                PlacePhotoMetadataBuffer photoMetadataBuffer = result.getPhotoMetadata();
+                if (photoMetadataBuffer.getCount() > 0 && !isCancelled()) {
+                    // Get the first bitmap and its attributions.
+                    PlacePhotoMetadata photo = photoMetadataBuffer.get(0);
+                    CharSequence attribution = photo.getAttributions();
+                    // Load a scaled bitmap for this photo.
+                    Bitmap image = photo.getScaledPhoto(mApiClient, mWidth, mHeight).await()
+                            .getBitmap();
+
+                    attributedPhoto = new AttributedPhoto(attribution, image);
+                }
+                // Release the PlacePhotoMetadataBuffer.
+                photoMetadataBuffer.release();
+            }
+            return attributedPhoto;
+        }
+
+        /**
+         * Holder for an image and its attribution.
+         */
+        class AttributedPhoto {
+
+            public final CharSequence attribution;
+
+            public final Bitmap bitmap;
+
+            public AttributedPhoto(CharSequence attribution, Bitmap bitmap) {
+                this.attribution = attribution;
+                this.bitmap = bitmap;
+            }
+        }
+    }
+
+    private void placePhotosTask(String placeId) {
+        // Create a new AsyncTask that displays the bitmap and attribution once loaded.
+        new PhotoTask(mImageView.getWidth(), mImageView.getHeight()) {
+            @Override
+            protected void onPreExecute() {
+                // Display a temporary image to show while bitmap is loading.
+//                mImageView.setImageResource(R.drawable.empty_photo);
+            }
+
+            @Override
+            protected void onPostExecute(AttributedPhoto attributedPhoto) {
+                if (attributedPhoto != null) {
+                    // Photo has been loaded, display it.
+                    mImageView.setImageBitmap(attributedPhoto.bitmap);
+
+//                    // Display the attribution as HTML content if set.
+//                    if (attributedPhoto.attribution == null) {
+//                        mText.setVisibility(View.GONE);
+//                    } else {
+//                        mText.setVisibility(View.VISIBLE);
+//                        mText.setText(Html.fromHtml(attributedPhoto.attribution.toString()));
+//                    }
+                }
+            }
+        }.execute(placeId);
     }
 }
